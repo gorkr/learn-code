e94c1bee8af3d97314f781b91c354551ffc65405
gorkr <17320010710@163.com>
2019-01-01 11:59:35 +0800

    a class implement a Stack

---
+++ python-data-structure/base.py
@@ -0,0 +1,20 @@
+class Stack:
+    """使用list实现栈"""
+
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        return self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[-1]
+
+    def size(self):
+        return len(self.items)
